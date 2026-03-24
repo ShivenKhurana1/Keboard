0b104573075031adfd3ac18eb49e5061c9db08a4
Shiven Khurana <shivenbk@gmail.com>
2026-03-23 21:08:43 -0400

    init

---
+++ Firmware/clear60/keymaps/default/keymap.c
@@ -0,0 +1,53 @@
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE,
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RGUI, KC_MENU, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, MO(_FN)
+    ),
+    [_FN] = LAYOUT(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT
+    )
+};
+
+#ifdef ENCODER_MAP_ENABLE
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FN]   = { ENCODER_CCW_CW(KC_PGDN, KC_PGUP) }
+};
+#endif
+
+#ifdef OLED_ENABLE
+static void render_layer_state(void) {
+    oled_write_ln_P(PSTR("Clear60"), false);
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case _FN:
+            oled_write_ln_P(PSTR("FN"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Base"), false);
+            break;
+    }
+}
+
+bool oled_task_user(void) {
+    oled_clear();
+    render_layer_state();
+    oled_write_P(PSTR("Caps: "), false);
+    oled_write_ln_P(host_keyboard_led_state().caps_lock ? PSTR("ON") : PSTR("OFF"), false);
+    return false;
+}
+#endif
---
+++ Firmware/clear60/readme.md
@@ -0,0 +1,23 @@
+# Clear60
+
+QMK firmware for the Clear60 RP2040 (Raspberry Pi Pico) keyboard.
+
+## Pins
+
+- Rows: GP14, GP15, GP16, GP17, GP18
+- Cols: GP0..GP13
+- Encoder: GP27 (A), GP26 (B)
+- OLED I2C: GP20 (SDA), GP21 (SCL)
+
+## Notes
+
+- `DIODE_DIRECTION` is set to `COL2ROW`. If your matrix is reversed, flip it in config.h.
+- The layout is a generic 5x14 grid. Adjust the keymap to match your physical layout.
+
+## Build
+
+Copy this folder into `qmk_firmware/keyboards/clear60`, then run:
+
+```
+qmk compile -kb clear60 -km default
+```
